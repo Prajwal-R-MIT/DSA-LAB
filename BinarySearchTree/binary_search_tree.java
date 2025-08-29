b3f2d41ac3668aa92ad8f93344d9e55cfc933f72
lab-pc <lab-pc>
2025-08-29 15:24:17 +0530

    Lab 4.

---
+++ BinarySearchTree/binary_search_tree.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Node
+{
+     
+}
+
+public class binary_search_tree 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello.");
+    }
+}
