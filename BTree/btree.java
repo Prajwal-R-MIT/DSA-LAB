2208f904b3cb0840acab01106aa0ccd243a1982d
lab-pc <lab-pc>
2025-09-05 15:43:18 +0530

    Lab 5

---
+++ BTree/btree.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class bTreeNode
+{
+    int[] Keys;
+    int Order;
+    bTreeNode[] Children;
+    int NumberOfNodes;
+    boolean Leaf;
+
+    public bTreeNode(int Order, boolean Leaf)
+    {
+        this.Keys = new int[Order - 1];
+        this.Order = Order;
+        this.Children = new bTreeNode[Order];
+        this.NumberOfNodes = 0;
+        this.Leaf = Leaf;
+    }
+}
+
+class bTree
+{
+    bTreeNode Root;
+
+    public bTree(int Order)
+    {
+        this.Root = new bTreeNode(Order, true);
+    }
+}
